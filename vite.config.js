e5967b1d90d0f5ba3fbae8f19ea59279a47a892a
Alsine Sanico <markalsine123@gmail.com>
2025-03-04 18:39:10 +0800

    cosvbdahs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
+      '@': '/resources/js',
+      '~': '/resources/css',
     },
   },
   build: {
